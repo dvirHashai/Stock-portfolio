980f6320b1fc3ce50d419f0f61eaf1e719534f05
dvirHashai <dvirh3@gmail.com>
2018-06-14 19:34:06 +0300

    Create stock controller's api

--- src/main/java/com/fyber/stockportfolio/controller/StockController.java
+++ src/main/java/com/fyber/stockportfolio/controller/StockController.java
@@ -1,15 +1,13 @@
 package com.fyber.stockportfolio.controller;
 
+import com.fyber.stockportfolio.common.response.dtos.StockPortfolioDto;
 import com.fyber.stockportfolio.common.response.dtos.UserDto;
 import com.fyber.stockportfolio.common.response.response.AbstractResponse;
 import com.fyber.stockportfolio.service.StockService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -33,11 +31,24 @@ public class StockController {
     /* --- Public methods --- */
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-        public AbstractResponse register(@RequestBody UserDto userDto){
-            Collection response = new ArrayList();
-
+    public AbstractResponse register(@RequestBody UserDto userDto) {
+        Collection response = new ArrayList();
+        //registerNew/existingUser
 
         return new AbstractResponse<>(response);
-        }
+    }
 
+    @RequestMapping(value = "/updateUserStockPortfolio", method = RequestMethod.POST)
+    public AbstractResponse updateUserStockPortfolio (@RequestBody StockPortfolioDto stockPortfolioDto){
+        Collection response = new ArrayList();
+
+        return new AbstractResponse(response);
+    }
+
+    @RequestMapping(value = "/getCurrentPortfolioValue/{userId}", method = RequestMethod.GET)
+    public AbstractResponse getCurrentPortfolioValue(@PathVariable(value = "userId") Integer userId){
+        Collection response = new ArrayList();
+
+        return new AbstractResponse(response);
+    }
 }
